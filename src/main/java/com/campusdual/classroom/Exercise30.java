cb6cb3fc7583feb599f58ccbd3d5d1dff347d6c2
Alex Domínguez <doalex7@gmail.com>
2024-11-22 09:10:14 +0100

    Ejercicio completado

--- src/main/java/com/campusdual/classroom/Exercise30.java
+++ src/main/java/com/campusdual/classroom/Exercise30.java
@@ -3,7 +3,13 @@ package com.campusdual.classroom;
 public class Exercise30 {
 
     public static int divisionWithCustomException(int dividend, int divisor){
-
+        int res = 0;
+    try{
+        res = dividend/divisor;
+    } catch (ArithmeticException e) {
+        throw new DivisionByZeroException();
+    }
+    return res;
     }
 
     public static void main(String[] args) {
